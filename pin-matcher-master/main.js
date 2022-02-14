8730e7684404adb9041baff1fa9f8aae8d890b46
muzahid604 <604muzahid@gmail.com>
2022-02-14 16:41:44 +0600

    calculation added

--- pin-matcher-master/main.js
+++ pin-matcher-master/main.js
@@ -1,3 +1,5 @@
+document.getElementById('error').style.display = 'none';
+document.getElementById('success').style.display = 'none';
 document.getElementById('generate-btn').addEventListener('click', function () {
     const generatePin = Math.round(Math.random() * 10000)
     const generateInput = document.getElementById('generate-input')
@@ -18,5 +20,20 @@ document.getElementById('calculator').addEventListener('click', function (event)
         const prevNumber = calcInput.value;
         const newNumber = prevNumber + number;
         calcInput.value = newNumber;
+
+    }
+});
+document.getElementById('submit-btn').addEventListener('click', function varifyPin() {
+    const generatePin = document.getElementById('generate-input').value;
+    const myPin = document.getElementById('calc-input').value
+    document.getElementById('calc-input').value = ''
+
+    if (generatePin == myPin) {
+        document.getElementById('error').style.display = 'none';
+        document.getElementById('success').style.display = 'block';
+    }
+    else {
+        document.getElementById('error').style.display = 'block';
+        document.getElementById('success').style.display = 'none';
     }
 })
